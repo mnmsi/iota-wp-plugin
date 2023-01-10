c46530ba9062c8687272037902cd65ab1059ff3d
HmHasan <mahmudul.iotait@gmail.com>
2023-01-10 13:50:48 +0600

    initial commit

---
+++ iota-init.php
@@ -0,0 +1,135 @@
+<?php
+/**
+ * Plugin Name:       Iota Init
+ * Plugin URI:        https://iotait.tech
+ * Description:       Install Necessary Assets
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Iota
+ * Author URI:        https://iotait.tech
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       iota-init
+ * Domain Path:       /iota-init
+ */
+
+/**
+ * Load Style And Script
+ */
+
+//load assets class
+
+require 'vendor/autoload.php';
+
+//require_once 'src/Assets.php';
+
+use App\Assets;
+
+if (class_exists('App\Assets')) {
+    $assets = new Assets();
+
+}
+
+register_activation_hook(__FILE__, array($assets, 'activate'));
+register_deactivation_hook(__FILE__, array($assets, 'deactivate'));
+
+/**
+ * initialize code star framework
+ */
+
+require_once "src/cs-framework/codestar-framework.php";
+// Control core classes for avoid errors
+if (class_exists('CSF')) {
+
+    //
+    // Set a unique slug-like ID
+    $prefix = 'iota_init';
+    //
+    /**
+     * Primary Theme Options
+     */
+    CSF::createOptions($prefix, array(
+        'menu_title' => 'Theme Options',
+        'menu_slug' => 'theme-options',
+        'menu_position' => 3,
+        'menu_icon' => 'dashicons-admin-generic',
+        'framework_title' => 'IOTA THEME  <small>by Iota</small>',
+        'footer_credit' => 'IOTA Theme  <small>by Iota</small>',
+        'show_bar_menu' => false,
+        'footer_text' => 'Thank you for using IOTA Theme',
+        'ajax_save' => true,
+        'show_reset_all' => false,
+        'show_search' => false,
+        'show_all_options' => false,
+        'show_reset_section'=> false,
+        'show_footer' => false,
+        'theme' => 'dark',
+        'class' => 'iota-init',
+        'sticky_header' => false,
+        'output_css' => false,
+    ));
+
+    /**
+     * Home Page
+     * @package iota init
+     */
+    CSF::createSection($prefix, array(
+        'id' => 'home_page',
+        'title' => 'Home Page',
+    ));
+    CSF::createSection($prefix, array(
+        'parent' => 'home_page',
+        'title' => 'Hero Section',
+        'fields' => array(
+            array(
+                'id' => 'hero_title',
+                'type' => 'text',
+                'title' => 'Hero Title',
+                'default' => 'IOTA',
+            ),
+            array(
+                'id' => 'hero_subtitle',
+                'type' => 'text',
+                'title' => 'Hero Subtitle',
+                'default' => 'IOTA',
+            ),
+            array(
+                'id' => 'bg_image',
+                'type' => 'media',
+                'title' => 'Background Image',
+                'library' => 'image',
+                'button_title' => 'Upload Image',
+            ),
+        ),
+    ));
+
+    /**
+     * About Page
+     * @package iota init
+     */
+
+    CSF::createSection($prefix, array(
+        'id' => 'about_page',
+        'title' => 'About Page',
+    ));
+    CSF::createSection($prefix, array(
+        'parent' => 'about_page',
+        'title' => 'About Section',
+        'fields' => array(
+            array(
+                'id' => 'about_title',
+                'type' => 'text',
+                'title' => 'About Title',
+                'default' => 'IOTA',
+            ),
+            array(
+                'id' => 'about_subtitle',
+                'type' => 'text',
+                'title' => 'About Subtitle',
+                'default' => 'IOTA',
+            ),
+        ),
+    ));
+}
---
+++ src/Assets.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+class Assets
+{
+    public function __construct()
+    {
+        add_action('wp_enqueue_scripts', array($this, 'iota_init'));
+    }
+
+    public function iota_init()
+    {
+        wp_register_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css', array(), '1.0.0', 'all');
+        wp_enqueue_style('bootstrap');
+        wp_register_script('bootstrap-popper', 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js', array(), '1.0.0', true);
+        wp_enqueue_script('bootstrap-popper');
+        wp_register_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js', array(), '1.0.0', true);
+        wp_enqueue_script('bootstrap');
+    }
+}
+
